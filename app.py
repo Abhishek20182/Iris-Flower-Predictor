3b122e0a93cb11440c2cdab5d1e029a1ba695f0c
Abhishek Tiwari <45140530+Abhishek20182@users.noreply.github.com>
2021-06-15 22:44:57 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #Python Libraries 
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import flask
 import pickle
-import joblib
+from pickle import load
 import numpy as np
 
 #ref: https://www.wintellect.com/creating-machine-learning-web-api-flask/
@@ -13,23 +13,24 @@ app = Flask(__name__)
 def index():
     return flask.render_template('index.html')
 
+
 filename = 'model.pkl'
-clf = joblib.load(open(filename, 'rb'))
+clf = load(open(filename, 'rb'))
 
 @app.route("/predict", methods=['POST'])
 def predict():
-	if request.method == "POST":
-		sepal_length = float(request.form['sepal_length'])
-		sepal_width = float(request.form['sepal_width'])
-		petal_length = float(request.form['petal_length'])
-		petal_width = float(request.form['petal_width'])
+    if request.method == "POST":
+        sepal_length = float(request.form['sepal_length'])
+        sepal_width = float(request.form['sepal_width'])
+        petal_length = float(request.form['petal_length'])
+        petal_width = float(request.form['petal_width'])
 
-		data = np.array([[sepal_length, sepal_width, petal_length, petal_width]]).reshape(1, -1)
-		my_prediction = clf.predict(data)
+        data = np.array([[sepal_length, sepal_width, petal_length, petal_width]]).reshape(1, -1)
+        my_prediction = clf.predict(data)
 
-		return flask.render_template('result.html', prediction=my_prediction)
-	else:
-		return flask.render_template('index.html')
+        return flask.render_template('result.html', prediction=my_prediction)
+    else:
+        return flask.render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
